013a0681916187a07c12d438983a9bd10e466850
Afzal123 <syed.afzal@codenvoi.com>
2018-01-11 12:54:20 +0500

    minor change

--- src/components/chatBox.jsx
+++ src/components/chatBox.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-
+import TextField from 'material-ui/TextField';
 import List, {
   ListItem,
   ListItemAvatar,
@@ -9,6 +9,8 @@ import List, {
   ListItemText,
 } from 'material-ui/List';
 import ListSubheader from 'material-ui/List/ListSubheader';
+import './style/Styles.css'
+
 const style = {
   height: '80vh',
   width: '86%',
@@ -18,7 +20,7 @@ const style = {
   chatHeader:{
     backgroundColor:"#009fff",
     height:'35px',
-    padding:'5px'
+    paddingTop:'5px'
   }
 };
 
@@ -53,24 +55,40 @@ class ChatBox extends Component{
     }
 
 render(){
+const chat_icon = require('../assets/PNG/png_menu_chat.png')
     return(
-        <div>
+        <div className="chat_box_container">
          <Paper style={style} zDepth={2}>
              <div className="chatHeader" style={style.chatHeader}>
-             {/* <Icon class="material-icons">chat_bubble_outline</Icon> */}
-             <span>CHATBOX</span>
+             <span className="chat-icon" >
+                    <img src={chat_icon} style={{width:'25px',height:'20px'}} />
+                  </span>
+             <span style={{color:'#FFFFFF',marginRight: '52px',marginLeft: '6px',fontSize: '13px'}}>CHATBOX</span>
       </div>     
-      <List> 
+      <List className="chat_list"> 
           {
               this.state.chats.map((data, index)=>(
                 <ListItemText
-                    style={{padding:'5px'}}
-                      primary={data.user}
-                      secondary={data.text}
+                key={index}
+                className="chat_content"
+                style={{padding:'5px'}}
+                primary={data.user}
+                secondary={data.text}
                     />
               ))
           }  
               </List>  
+        <form>
+                <TextField
+            id="full-width"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            style={{border:'1px solid grey'}}
+            placeholder="login to write message"
+            margin="normal"
+        />
+      </form>
          </Paper>
         </div>
     )
