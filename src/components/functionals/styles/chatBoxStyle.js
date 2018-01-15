d2865325eba1409f04e536073a0a0dad720f449e
Afzal123 <syed.afzal@codenvoi.com>
2018-01-15 16:33:39 +0500

    minor changes in chatbox

--- src/components/functionals/styles/chatBoxStyle.js
+++ src/components/functionals/styles/chatBoxStyle.js
@@ -5,56 +5,76 @@ const primary = palette.primary;
 const grey = palette.textLignt;
 
 export default {
-  chat_box_container: {
-    padding: '10px',
-  },
-  footerWraper: {
-    position: 'absolute',
-    bottom: '0px',
-  },
-  chatHeader: {
-    backgroundColor: primary,
-    height: '35px',
-    paddingTop: '7px',
-    textAlign: 'left',
-    paddingLeft: '10px',
-  },
-  chatIcon: {
-    width: '25px',
-    height: '20px',
-  },
-  chatHeaderText: {
-    color: white,
-    marginRight: '52px',
-    marginLeft: '6px',
-    fontSize: '13px',
-  },
-  chatList: {
-    height: 'calc(100vh - 230px)',
-    overflowY: 'auto',
-  },
-  chatContent: {
-    padding: '10px',
-    textAlign: 'left',
-    marginLeft: '10px',
-    display: 'block',
-  },
-  textBox: {
-    border: 'thin solid ' + grey,
-    backgroundColor: white,
-    '&>div': {
-      width: 'calc(100% - 40px)',
-      marginLeft: '20px',
-    },
-    '& ::before': {
-      backgroundColor: 'transparent',
-    },
-  },
-  primaryText: {
-    color: primary,
-  },
-  secondaryText: {
-    color: grey,
-    padding: '0px 0px 0px 5px',
-  },
+  root: {
+
+    '& .chat_box_container': {
+      padding: '10px',
+      footerWraper: {
+        position: 'absolute',
+        bottom: '0px',
+      },
+      '& .paper-style': {
+        textAlign: 'center',
+        display: 'inline-block',
+        position: 'relative',
+        minWidth: '95%',
+        '& .chatbox-Header': {
+          backgroundColor: primary,
+          height: '35px',
+          paddingTop: '7px',
+          textAlign: 'left',
+          paddingLeft: '10px',
+          '& .chatBox-headerIcon': {
+            width: '25px',
+            height: '20px',
+          },
+          '& .chatBox-headerText': {
+            color: white,
+            marginRight: '52px',
+            marginLeft: '6px',
+            fontSize: '13px',
+          }
+
+        },
+        '& .list': {
+          maxHeight: '80%',
+          '& .chat-list': {
+            height: 'calc(100vh - 230px)',
+            overflowY: 'auto',
+            '& .chatContent-listItemText': {
+              padding: '10px',
+              textAlign: 'left',
+              marginLeft: '10px',
+              display: 'block',
+            },
+            '& .chatContent-primaryText': {
+              color: primary,
+            },
+            '& .chatContent-secondaryText': {
+              color: grey,
+              padding: '0px 0px 0px 5px',
+            }
+          }
+        }
+        , '& .form': {
+          border: 'thin solid ' + grey,
+          backgroundColor: white,
+          '&>div': {
+            width: 'calc(100% - 60px)',
+            // marginLeft: '20px',
+          },
+          '& ::before': {
+            backgroundColor: 'transparent',
+          },
+          '& .send-button':{
+            position:'absolute',
+            marginTop : '3px',
+            cursor:'pointer',
+            color:'#a8aba5'
+          }
+        }
+      }
+    }
+  }
+
 };
