0f71c232e15f2e59b124dc8a9316ec04f9952aed
cortesa <cortesa@gmail.com>
2018-01-16 18:34:24 +0100

    Minors changes to make it work

--- src/components/pages/styles/index.js
+++ src/components/pages/styles/index.js
@@ -2,9 +2,6 @@ import palette from './palette';
 import registerStyle from './registerStyle';
 import loginStyle from './loginStyle';
 import dashboardStyle from './dashboardStyle';
-
-export { palette, registerStyle, loginStyle ,dashboardStyle };
-
 import userAccountStyle from './userAccountStyle';
 
-export { palette, registerStyle, loginStyle,userAccountStyle };
+export { palette, registerStyle, loginStyle, userAccountStyle, dashboardStyle };
