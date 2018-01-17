1679dca3e069d85c45049a95ffb66367e20327a0
cortesa <cortesa@gmail.com>
2018-01-17 18:14:34 +0100

    Stat Dumb data

--- src/redux/reducers/sysStatsReducer.js
+++ src/redux/reducers/sysStatsReducer.js
@@ -16,25 +16,55 @@ const initialState = {
       btc_dash: '0.06870001',
     },
   },
-  valueOld: {},
-  values: [
+  valueOld: {
+    general: {
+      consensus_blockheight: '805694',
+      consensus_version: '120202',
+      consensus_protocolversion: '70208',
+      all_user: '3648',
+      registered_masternodes: '3036',
+      registered_masternodes_verified: '1087',
+    },
+    exchange_rates: {
+      dash_usd: 712.4703203664,
+      btc_usd: '10502.64',
+      btc_dash: '0.06783726',
+    },
+  },
+  valueOldCopy: {
+    general: {
+      consensus_blockheight: '805694',
+      consensus_version: '120202',
+      consensus_protocolversion: '70208',
+      all_user: '3648',
+      registered_masternodes: '3036',
+      registered_masternodes_verified: '1087',
+    },
+    exchange_rates: {
+      dash_usd: 712.4703203664,
+      btc_usd: '10502.64',
+      btc_dash: '0.06783726',
+    },
+  },
+  valueDiff: {},
+  cards: [
     {
       img: 'png_stasts_sys.png',
-      num: '0.00022',
-      text: 'BTC 1000 USD',
+      num: 'exchange_rates.btc_usd',
+      text: 'BTC/1000 USD',
       percentage: '9%',
       arrow: 'png_button_down.png',
     },
     {
       img: 'png_menu_masternodes_selected.png',
-      num: '0415/0430',
-      text: 'REGISTERED MASTER NODES',
+      num: 0,
+      text: 'REGISTERED MASTERNODES',
       percentage: '10%',
       arrow: 'png_button_up.png',
     },
     {
       img: 'png_stats_users.png',
-      num: '2000',
+      num: 0,
       text: 'ALL USERS',
       percentage: '9%',
       arrow: 'png_button_down.png',
