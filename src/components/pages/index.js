a4bb52ba377d31cb804d808ba40e07d7d0b781f6
cortesa <cortesa@gmail.com>
2018-01-23 16:05:37 +0100

    ignore yarn.lock

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -3,11 +3,21 @@ import Login from './Login';
 import Register from './Register';
 import DashBoard from './DashBoard';
 import Faq from './Faq';
-import MasterNode from './MasterNode';
+import MasternodeSetting from './MasternodeSetting';
 import NewProposal from './NewProposal';
 import News from './News';
 import UserAccount from './UserAccount';
 
 //Import testing components
 
-export { Home, Login, Register, DashBoard, Faq, MasterNode, NewProposal, News, UserAccount };
+export {
+  Home,
+  Login,
+  Register,
+  DashBoard,
+  Faq,
+  MasternodeSetting,
+  NewProposal,
+  News,
+  UserAccount,
+};
