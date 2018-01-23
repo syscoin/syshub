0871c8345aac9a731ed8c5e5816d533a0103e3e7
Afzal123 <syed.afzal@codenvoi.com>
2018-01-23 17:52:26 +0500

    prosposal title and details(editor) step complete

--- src/components/pages/NewProposal.jsx
+++ src/components/pages/NewProposal.jsx
@@ -4,14 +4,18 @@ import { connect } from 'react-redux';
 import actions from '../../redux/actions';
 import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui';
-
+import { Row, Col, Card } from 'antd';
+import { Form, Icon, Input, Button } from 'antd';
 //import style
 import NewProposalStyle from './styles/newProposalStyle'
 // import components
-import { Stats, WelcomeBox } from '../functionals';
+import { Editor } from 'react-draft-wysiwyg';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+
+
+const FormItem = Form.Item;
 class NewProposal extends Component {
-
-
 
   render() {
     const { classes } = this.props;
@@ -19,6 +23,57 @@ class NewProposal extends Component {
       <div className={classes.root}>
         <h1 className='title'>Proposal Configuration</h1>
         <Paper className='paper-container' elevation={4}>
+          {/* Proposal Title Row */}
+          <Row className="proposal-title-row">
+            {/* Proposal Title Colomn */}
+            <Col span={10}>
+              {/* Proposal heading */}
+              <h1 className="proposal-title"> <span className="proposalHeading-dot"></span> Proposal Title</h1>
+              {/* proposal title input field */}
+              <Form>
+                <FormItem className='form-item'>
+                  <Input placeholder="Insert Reference Title" />
+                </FormItem>
+              </Form>
+            </Col>
+            {/* Proposal Description Url Colomn */}
+            <Col span={14}>
+              {/* Proposal description heading */}
+              <h1 className="proposal-title">Proposal Discription Url</h1>
+              <span className="proposal-description-url">http://www.syshub.com/p/proposal-title</span>
+              <div className="nextStep-button-div">
+                <Button type="primary">Next Step</Button>
+              </div>
+            </Col>
+
+          </Row>
+
+          {/* Proposal Detail Row */}
+          <Row className="proposal-details-row">
+            {/* Proposal Detail Colomn */}
+            <Col span={20}>
+              {/* Proposal heading */}
+              <h1 className="proposal-title"> <span className="proposalHeading-dot"></span> Proposal Detials</h1>
+              <Button className='preview-edit-button'>preview</Button>
+              <h2 className="editor-title">Write proposal details</h2>
+              <Editor
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="proposalEditor-wrapper"
+                editorClassName="proposal-editor"
+                  toolbar={{
+                  options: ['inline', 'list'],
+                  inline: {
+                  options: ['bold', 'italic', 'underline', 'monospace'],
+                  list: {
+                  options: ['unordered', 'ordered'],
+                      },
+                    },
+                }} />
+                <Button className='confirm-button'>Confirm</Button>
+            </Col>
+          </Row>
+
+
 
         </Paper>
       </div>
--- src/components/pages/styles/newProposalStyle.js
+++ src/components/pages/styles/newProposalStyle.js
@@ -1,7 +1,9 @@
 import palette from './palette';
 
 const primary = palette.primary;
+const primaryLight = palette.primaryLight
 const white = palette.white;
+const grey = palette.grey;
 const greyDark = palette.greyDark;
 const greyLight = palette.greyLight;
 const secondary = palette.secondary;
@@ -12,13 +14,102 @@ export default {
         '& .title': {
             fontWeight: 'lighter',
             color: greyDark,
-            fontSize:20
+            fontSize: 20
         },
         '& .paper-container': {
             padding: '20px 20px',
+            '& .proposal-title': {
+                fontWeight: 100,
+                fontSize: '22px',
+                color: primaryLight,
+                display: 'inline-block'
+            },
+            '& .proposalHeading-dot': {
+                backgroundColor: primary,
+                borderRadius: '10px',
+                height: '20px',
+                width: '20px',
+                display: 'inline-block',
+                marginRight: "15px"
+            },
+            '& .proposal-title-row': {
+                '& .form-item': {
+                    marginLeft: '30px',
+                    '& Input': {
+                        width: '85%',
+                        height: '40px'
+                    }
+                },
+                '& .proposal-description-url': {
+                    display: 'block',
+                    marginLeft: '25px',
+                    color: grey
+                },
+                '& .nextStep-button-div': {
+                    textAlign: 'center',
+                    margin: '40px 0px 0px 0px',
+                    '& Button': {
+                        borderRadius: '7px',
+                        height: '35px'
+                    },
+                    '& button span': {
+                        textTransform: 'capitalize',
+                        padding: '3px 0px',
+                        fontSize: '18px'
+                    },
+                }
+            },
+            '& .proposal-details-row': {
+                '& .preview-edit-button': {
+                    marginLeft: '20px',
+                    height: '30px',
+                    borderRadius: '8px'
+                },
+                '& .editor-title': {
+                    position: 'absolute',
+                    marginLeft: 60,
+                    fontSize: 18,
+                    marginTop: 30,
+                    fontWeight: 'bold',
+                    color: greyDark
+                },
+                '& .proposal-editor': {
 
+                    padding: '15px',
+                    marginTop: '10px',
+                    backgroundColor: white,
+                    height: 100
 
-        }
+                },
+                '& .proposalEditor-wrapper': {
+                    marginLeft: '40px',
+                    backgroundColor: greyLight,
+                    marginTop: 20,
+                    boxShadow: 'rgba(0, 0, 0, 0.20) 0px 5px 20px',
+                    height: 200
+                },
+                '& .toolbarClassName': {
+                    visibility: 'visible',
+                    width: 300,
+                    marginLeft: 'calc(100% - 300px)',
+                    display: 'inline-flex',
+                    backgroundColor: greyLight
+
+                },
+                '& .confirm-button': {
+                    marginLeft: 'calc(100% - 105px)',
+                    marginTop: -45,
+                    position: 'absolute',
+                    borderRadius: 6,
+                    backgroundColor: grey,
+
+                },
+                '& .confirm-button span': {
+                    color: white
+                }
+            }
+        },
+
     }
+}
 
-};
