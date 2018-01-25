5dd9104c2e2848f5498111b29296ca8ef50f0fb5
Taylor <tbaustin1992@gmail.com>
2018-01-25 14:32:04 -0600

    proposalCard attached to proposal list

--- src/components/functionals/ProposalCard.jsx
+++ src/components/functionals/ProposalCard.jsx
@@ -16,17 +16,13 @@ import { proposalCardStyle } from './styles';
 class ProposalCard extends Component {
   voteUp(vote) {
     if (this.props.user) {
-      console.log(
-        this.props.user.displayName + ' Voted For Up ' + ' for id ' + vote.id
-      );
+      console.log(this.props.user.displayName + ' Voted For Up ' + ' for id ' + vote.id);
     }
   }
 
   voteDown(vote) {
     if (this.props.user) {
-      console.log(
-        this.props.user.displayName + ' Voted For Down ' + ' for id ' + vote.id
-      );
+      console.log(this.props.user.displayName + ' Voted For Down ' + ' for id ' + vote.id);
     }
   }
 
@@ -38,29 +34,22 @@ class ProposalCard extends Component {
     const voteDownIcon = require('../../assets/img/png_button_down.png');
 
     // Some Maths ;P
-    const progress =
-      parseInt(proposal.upVote) / parseInt(this.props.totalNodes) * 100;
+    const progress = parseInt(proposal.YesCount) / parseInt(this.props.totalNodes) * 100;
 
     return (
       <Grid container className={classes.proposalRoot}>
-        <Grid container md={12} className="proposalRow" key={proposal.id}>
+        <Grid container md={12} className="proposalRow" key={proposal.Hash}>
           <Grid item md={2} className="proposalView">
             <Progress
               type="circle"
               percent={progress}
-              format={percent => (
-                <img alt="a" src={docIcon} className="progressIcon" />
-              )}
+              format={percent => <img alt="a" src={docIcon} className="progressIcon" />}
               className="progress-dial"
               strokeWidth={12}
-              status={
-                progress < 25
-                  ? 'exception'
-                  : progress < 50 ? 'active' : 'success'
-              }
+              status={progress < 25 ? 'exception' : progress < 50 ? 'active' : 'success'}
             />
             <div className="proposalStatusNo">
-              <span className="proposalStatusActiveNo">{proposal.upVote}</span>
+              <span className="proposalStatusActiveNo">{proposal.YesCount}</span>
               {' / '}
               {this.props.totalNodes}
             </div>
@@ -70,14 +59,18 @@ class ProposalCard extends Component {
             {/* <Button className={ proposal.active ? 'activeVoteButton' : 'voteButton' }>
                   Vote on Proposal
                 </Button> */}
-            <h1
-              className="proposalHeading"
-              onClick={() => selectProposal(proposal.id)}
-            >
-              {' '}
-              {proposal.name}
+            <h1 className="proposalHeading" onClick={() => selectProposal(proposal.Hash)}>
+              {proposal.DataString[0][1].name ? (
+                proposal.DataString[0][1].name.split('\n', 1)[0]
+              ) : (
+                <span style={{ color: 'grey' }}>No name available for this proposal.</span>
+              )}
             </h1>
-            <div className="proposalDetail">{proposal.detail}</div>
+            <div className="proposalDetail">
+              {proposal.DataString[0][1].description
+                ? `${proposal.DataString[0][1].description.substr(0, 120)}...`
+                : 'No description available for this proposal.'}
+            </div>
           </Grid>
 
           {user ? (
@@ -86,26 +79,23 @@ class ProposalCard extends Component {
               <Button className="vote-up" onClick={() => this.voteUp(proposal)}>
                 <img src={voteUpIcon} className="upVoteIcon" alt="" />
               </Button>
-              <Button
-                className="vote-down"
-                onClick={() => this.voteDown(proposal)}
-              >
+              <Button className="vote-down" onClick={() => this.voteDown(proposal)}>
                 <img src={voteDownIcon} className="downVoteIcon" alt="" />
               </Button>
               <div className="vote-count">
-                <div className="vote-number">{proposal.upVote}</div>
-                <div className="vote-number">{proposal.downVote}</div>
+                <div className="vote-number">{proposal.YesCount}</div>
+                <div className="vote-number">{proposal.NoCount}</div>
               </div>
             </Grid>
           ) : (
             <Grid item md={3} className="vote__wrapper">
               <div className="vote-up">
                 <img alt="a" src={voteUpIcon} className="smallUpVoteIcon" />
-                <span className="voteNumber">{proposal.upVote}</span>
+                <span className="voteNumber">{proposal.YesCount}</span>
               </div>
               <div className="vote-down">
                 <img alt="a" src={voteDownIcon} className="smallDownVoteIcon" />
-                <span className="voteNumber">{proposal.downVote}</span>
+                <span className="voteNumber">{proposal.NoCount}</span>
               </div>
             </Grid>
           )}
@@ -117,7 +107,7 @@ class ProposalCard extends Component {
 
 const stateToProps = state => {
   return {
-    user: state.app.currentUser,
+    user: state.app.currentUser
   };
 };
 
@@ -125,6 +115,4 @@ const dispatchToProps = dispatch => {
   return {};
 };
 
-export default connect(stateToProps, dispatchToProps)(
-  withStyles(proposalCardStyle)(ProposalCard)
-);
+export default connect(stateToProps, dispatchToProps)(withStyles(proposalCardStyle)(ProposalCard));
