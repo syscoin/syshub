10c28850e3dc70d002a7411ed0006337b2d0a715
cortesa <cortesa@gmail.com>
2018-02-03 01:54:49 +0100

    Minor changes

--- src/components/containers/styles/mHeaderNavStyle.js
+++ src/components/containers/styles/mHeaderNavStyle.js
@@ -4,26 +4,27 @@ const primary = palette.primary;
 const primaryDark = palette.primaryDark;
 const primaryLight = palette.primaryLight;
 const white = palette.white;
+const gery = palette.grey;
 
 export default {
-    mRoot: {
-        borderTop: 'thin solid ' + white,
-        marginTop: 25,
-        '& .divider': {
-            height: '100%',
-            margin: 0
-        },
-        '& .center-section': {
-            textAlign: 'center',
-            borderLeft: 'thin solid ' + white,
-            borderRight: 'thin solid ' + white,
-            height: 70,
-            '& img': {
-                height: 50
-            }
-        },
-        '& button': {
-            border: 'none'
-        }
+  mRoot: {
+    borderTop: 'thin solid ' + gery,
+    marginTop: 25,
+    '& .divider': {
+      height: '100%',
+      margin: 0
+    },
+    '& .center-section': {
+      textAlign: 'center',
+      borderLeft: 'thin solid ' + gery,
+      borderRight: 'thin solid ' + gery,
+      height: 70,
+      '& img': {
+        height: 50
+      }
+    },
+    '& button': {
+      border: 'none'
     }
-}
+  }
+};
