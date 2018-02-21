2ebe91c8c841ab2cfc1cfb16b255779102dc1a02
kashifahmed91 <kashif91ahmed@gmail.com>
2018-02-21 20:06:26 +0500

    Fixed the height of welcome box image

--- src/components/functionals/styles/welcomeBoxStyle.js
+++ src/components/functionals/styles/welcomeBoxStyle.js
@@ -23,7 +23,7 @@ export default {
       '& .logoDiv': {
         textAlign: 'center',
         '& img': {
-          width: 200
+          width: 300
         }
       },
       '& .addvertiseText': {
@@ -52,7 +52,7 @@ export default {
     '& .tile__wrapper': {
       height: '100% !important',
       '&.logo': {
-        marginTop: 55
+        marginTop: 25
       }
     }
   },
