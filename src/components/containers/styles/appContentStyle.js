6ab8c8c50fbe5bea43619b6b72974942f1e2e63e
cortesa <cortesa@gmail.com>
2018-10-11 20:02:23 +0200

    Hide Welcome Section and center the stats cards

--- src/components/containers/styles/appContentStyle.js
+++ src/components/containers/styles/appContentStyle.js
@@ -3,18 +3,27 @@ import palette from './palette';
 const greyLight = palette.greyLight;
 
 export default {
-  wraper: {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    background: greyLight,
+    padding: '0px',
+  },
+  contentWrapper: {
     display: 'flex',
     flexDirection: 'column',
-    justifyAlign: 'flex-start',
-    flex: '2 1 auto',
+    justifyContent: 'center',
     background: greyLight,
     minWidth: '100%',
     padding: '0px',
-
-  }
-  ,
-  contentWraper: {
-    marginTop: '40px'
+  },
+  wrapperHome: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    minHeight: '80%',
+    background: greyLight,
+    padding: '0px',
   }
 };
