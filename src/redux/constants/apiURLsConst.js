2a9cfbaf124c6eeabe77e07389ee5a822f1915d1
cortesa <cortesa@gmail.com>
2018-10-12 14:33:11 +0200

    not secure connection to API

--- src/redux/constants/apiURLsConst.js
+++ src/redux/constants/apiURLsConst.js
@@ -4,5 +4,5 @@ export default {
   // URL_SYS_MN_API:'http://159.89.141.35:3000',  // Old net, no SYS available
   // URL_SYS_MN_API : 'http://159.89.151.42:3000', // New net
   // URL_SYS_MN_API: 'https://www.qnguyen.xyz' // Quang HTTPS server
-  URL_SYS_MN_API: 'https://206.189.219.222' // ACZ_Droplet HTTPS server
+  URL_SYS_MN_API: 'http://206.189.219.222' // ACZ_Droplet HTTPS server
 };
