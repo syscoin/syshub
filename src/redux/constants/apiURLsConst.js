461197e591a429369977e682a943d2334090cc75
cortesa <cortesa@gmail.com>
2018-11-08 20:49:24 +0100

    new production and develop API

--- src/redux/constants/apiURLsConst.js
+++ src/redux/constants/apiURLsConst.js
@@ -5,5 +5,6 @@ export default {
   // URL_SYS_MN_PROD_API : 'http://159.89.151.42:3000', // New net
   // URL_SYS_MN_PROD_API: 'https://www.qnguyen.xyz' // Quang HTTPS server
   // URL_SYS_MN_PROD_API: 'http://206.189.219.222' // ACZ_Droplet HTTPS server
-  URL_SYS_MN_PROD_API: 'https://sysapiprod.cortesa.net' // ACZ_Droplet HTTPS server
+  // URL_SYS_MN_PROD_API: 'https://sysapiprod.cortesa.net', // ACZ_Droplet HTTPS server
+  // URL_SYS_MN_DEV_API: 'https://sysapiprod.cortesa.net' // ACZ_Droplet HTTPS server
 };
