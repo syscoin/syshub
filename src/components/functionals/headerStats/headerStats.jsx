d7d121e8c5e841979661a08bc36addd60c60d863
cortesa <cortesa@gmail.com>
2019-11-28 18:18:27 +0100

    also in NavBar

--- src/components/functionals/headerStats/headerStats.jsx
+++ src/components/functionals/headerStats/headerStats.jsx
@@ -1,33 +1,49 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux'; //to pass functions
-import actions from '../../../redux/actions';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux"; //to pass functions
+import actions from "../../../redux/actions";
 //import antd components
-import { Grid } from '@material-ui/core';
+import { Grid } from "@material-ui/core";
 
-import injectSheet from 'react-jss';
-import headerStatsStyle from './headerStats.style';
+import injectSheet from "react-jss";
+import headerStatsStyle from "./headerStats.style";
 
 class HeaderStats extends Component {
   render() {
     const { classes, deviceType, sysInfo } = this.props;
     //Platform style switcher
-    const style = deviceType === 'mobile' ? classes.mRoot : classes.root;
+    const style = deviceType === "mobile" ? classes.mRoot : classes.root;
 
-    const usdChangeRate = sysInfo.sysPrice ? `${parseFloat(sysInfo.sysPrice.price_usd).toFixed(8)} USD` : '';
-    const btcChangeRate = sysInfo.sysPrice ? `${parseFloat(sysInfo.sysPrice.price_btc).toFixed(8)} BTC` : '';
-    const satChangeRate = sysInfo.sysPrice ? `${Math.floor(parseFloat(sysInfo.sysPrice.price_btc).toFixed(8) * 100000000)} SATOSHI` : '';
-    const masternodes   = sysInfo.mnRegistered && sysInfo.mnCount ? `${sysInfo.mnRegistered} / ${sysInfo.mnCount.enabled}` : '';
-    const totUsers      = sysInfo.users ? (sysInfo.users) : '';
-    const sysCaption    = deviceType === 'mobile' ? ':': 'SYSCOIN:';
+    const usdChangeRate = sysInfo.sysPrice
+      ? `${parseFloat(sysInfo.sysPrice.usd).toFixed(8)} USD`
+      : "";
+    const btcChangeRate = sysInfo.sysPrice
+      ? `${parseFloat(sysInfo.sysPrice.btc).toFixed(8)} BTC`
+      : "";
+    const satChangeRate = sysInfo.sysPrice
+      ? `${Math.floor(
+          parseFloat(sysInfo.sysPrice.btc).toFixed(8) * 100000000
+        )} SATOSHI`
+      : "";
+    const masternodes =
+      sysInfo.mnRegistered && sysInfo.mnCount
+        ? `${sysInfo.mnRegistered} / ${sysInfo.mnCount.enabled}`
+        : "";
+    const totUsers = sysInfo.users ? sysInfo.users : "";
+    const sysCaption = deviceType === "mobile" ? ":" : "SYSCOIN:";
     //console.clear();
     return (
       <Grid container className={style}>
-        <Grid item className="common" xs={deviceType === 'mobile' ? 12 : null}>
+        <Grid item className="common" xs={deviceType === "mobile" ? 12 : null}>
           <div className="changeRate">
-            <img alt="a" src={require('../../../assets/img/png_stasts_sys.png')} className="icon" onClick={() => this.props.setPage('home')}/>
+            <img
+              alt="a"
+              src={require("../../../assets/img/png_stasts_sys.png")}
+              className="icon"
+              onClick={() => this.props.setPage("home")}
+            />
             <span className="TxtBold">{sysCaption}</span>
             <div className="changeValue">
               <i>{usdChangeRate}</i>
@@ -36,22 +52,26 @@ class HeaderStats extends Component {
             </div>
           </div>
         </Grid>
-        <Grid item className="common" xs={deviceType === 'mobile' ? 12 : null}>
+        <Grid item className="common" xs={deviceType === "mobile" ? 12 : null}>
           <img
             alt="a"
-            src={require('../../../assets/img/png_stats_masternodes.png')}
+            src={require("../../../assets/img/png_stats_masternodes.png")}
             className="icon"
           />
-          {deviceType === 'mobile' ? (
+          {deviceType === "mobile" ? (
             <span className="TxtBold">{`: `}</span>
           ) : (
             <span className="TxtBold">{`REGISTERED MASTERNODES: `}</span>
           )}
           {masternodes}
         </Grid>
-        <Grid item className="common" xs={deviceType === 'mobile' ? 12 : null}>
-          <img alt="a" src={require('../../../assets/img/png_stats_users.png')} className="icon" />
-          {deviceType === 'mobile' ? (
+        <Grid item className="common" xs={deviceType === "mobile" ? 12 : null}>
+          <img
+            alt="a"
+            src={require("../../../assets/img/png_stats_users.png")}
+            className="icon"
+          />
+          {deviceType === "mobile" ? (
             <span className="TxtBold">{`: `}</span>
           ) : (
             <span className="TxtBold">{`USERS: `}</span>
@@ -74,8 +94,8 @@ function mapStateToProps(state) {
       mnCount: state.sysStats.mnCount,
       mnRegistered: state.sysStats.mnRegistered,
       sysPrice: state.sysStats.sysPrice,
-      users: state.sysStats.users,
-    },
+      users: state.sysStats.users
+    }
   };
 }
 
@@ -86,6 +106,7 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-  injectSheet(headerStatsStyle)(HeaderStats)
-);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(injectSheet(headerStatsStyle)(HeaderStats));
