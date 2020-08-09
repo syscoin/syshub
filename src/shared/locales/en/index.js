8d9e8b4af45f4d64551a7a2f3ec0caafcf2ddf36
Fibii <charafbahria@gmail.com>
2020-08-09 21:54:50 +0100

    add english translation for Investment

--- src/shared/locales/en/index.js
+++ src/shared/locales/en/index.js
@@ -2,10 +2,12 @@ import home from './home/index';
 import banner from './banner/index';
 import doughnut from './partials/doughnut/index';
 import income from './partials/income/index';
+import investment from './partials/investment/index';
 
 export default {
   ...home,
   ...banner,
   ...doughnut,
   ...income,
+  ...investment,
 }
---
+++ src/shared/locales/en/partials/investment/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default {
+  investment: {
+    investmentTable: {
+      title: 'Investment Stats',
+      label: 'Label',
+      value: 'Value',
+      coinsRequired: 'Coins Required',
+      masterNodePrice: 'Masternode Price',
+      roi: 'ROI',
+      payoutFrequency: 'Approx. Payout Frequency',
+      firstPayment: 'Approx. First Payment',
+      rewardAbility: 'Approx. Reward Eligibility',
+    },
+    blockchainTable: {
+      title: 'Blockchain Stats',
+      label: 'Label',
+      value: 'Value',
+      version: 'Version',
+      subVersion: 'Sub Version',
+      protocol: 'Protocol',
+      connections: 'Connections',
+      genesisBlock: 'Genesis Block',
+      averageBlockTime: 'Average Block Time',
+    },
+    loading: 'Data is being loaded',
+  },
+}
