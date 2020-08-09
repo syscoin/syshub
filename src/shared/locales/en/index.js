55f2ca189e6e2e81e1bb8ee8bf7a343d5a5e5f91
Fibii <charafbahria@gmail.com>
2020-08-09 21:32:38 +0100

    add english translation for Income

--- src/shared/locales/en/index.js
+++ src/shared/locales/en/index.js
@@ -1,9 +1,11 @@
 import home from './home/index';
 import banner from './banner/index';
 import doughnut from './partials/doughnut/index';
+import income from './partials/income/index';
 
 export default {
   ...home,
   ...banner,
   ...doughnut,
+  ...income,
 }
---
+++ src/shared/locales/en/partials/income/index.js
@@ -0,0 +1,16 @@
+export default {
+  income: {
+    title: 'Income Stats',
+    tabs: {
+      latestStats: 'Latest Stats',
+      oneYearSeniority: '1 Year Seniority',
+    },
+    income: {
+      daily: 'Daily Income',
+      weekly: 'Weekly Income',
+      monthly: 'Monthly Income',
+      yearly: 'Yearly Income',
+    },
+    loading: 'Data is being loaded',
+  },
+}
