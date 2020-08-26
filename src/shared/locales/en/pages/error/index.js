0bbfa57a6858de182c9161559884436eac4c6d1d
bigpoppa-sys <48267173+bigpoppa-sys@users.noreply.github.com>
2020-08-26 17:51:29 +1000

    Update index.js

--- src/shared/locales/en/pages/error/index.js
+++ src/shared/locales/en/pages/error/index.js
@@ -5,6 +5,6 @@ export default {
         keywords: 'Syscoin, Masternodes, Blockchain, Crypto, Blockmarket, Coins, Bitcoin, Cryptocurrency, Rewards',
         description: 'Sysnode.info provides Syscoin Masternode Operators the tools to maximise the most from their Masternodes!',
       },
-      title: 'The page you are looking for not found.'
+      title: 'Oops! The page you are looking for cannot be found.'
     }
 }
