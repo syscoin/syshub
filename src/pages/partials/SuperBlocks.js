4b0d65ce0581f2357541767109f32c8cb205a5fc
bigpoppa-sys <48267173+bigpoppa-sys@users.noreply.github.com>
2020-08-27 10:30:41 +1000

    Update SuperBlocks.js

--- src/pages/partials/SuperBlocks.js
+++ src/pages/partials/SuperBlocks.js
@@ -24,6 +24,43 @@ export class SuperBlocks extends Component {
             <section className="section__Income__investment gradient_box2 pt-0">
             <div className="container-fluid">
                 <div className="row">
+                    <div className="col-12 col-lg-6 animation" data-animation="fadeInUp" data-animation-delay="1.1s">
+                        <div className="Heading__Bar mb-3">
+                            <h2 className="text-white font-weight-normal text-center">{t('superblocks.govdetailtable.title')}</h2>
+                        </div>
+                        <div className="table-responsive">
+                            <table className="table live__table table-bordered table-dark table-striped">
+                                <thead className="table-heading">
+                                    <tr>
+                                        <th>{t('superblocks.govdetailtable.label')}</th>
+                                        <th>{t('superblocks.govdetailtable.value')}</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="table-body">
+                                    <tr>
+                                        <td>{t('superblocks.govdetailtable.current_superblock')}</td>
+                                        <td>{this.state.superBlockData.next_superblock}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('superblocks.govdetailtable.superblock_budget')}</td>
+                                        <td>{this.state.superBlockData.budget}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('superblocks.govdetailtable.requested_superblock_budget')}</td>
+                                        <td>{this.state.budgetSum}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('superblocks.govdetailtable.superblock_date_utc')}</td>
+                                        <td>{this.state.superBlockData.superblock_date}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('superblocks.govdetailtable.voting_deadline_utc')}</td>
+                                        <td>{this.state.superBlockData.voting_deadline}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                     <div className="col-12 col-lg-6 animation mb-5 mb-lg-0" data-animation="fadeInUp" data-animation-delay="1s">
                         <div className="Heading__Bar mb-3">
                             <h2 className="text-white font-weight-normal text-center">{t('superblocks.nextsuperblockstable.title')}</h2>
@@ -62,43 +99,6 @@ export class SuperBlocks extends Component {
                                         <td>{this.state.superBlockData.sb5}</td>
                                         <td>{this.state.superBlockData.sb5Budget+' SYS'}</td>
                                         <td>{this.state.superBlockData.sb5Date}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div className="col-12 col-lg-6 animation" data-animation="fadeInUp" data-animation-delay="1.1s">
-                        <div className="Heading__Bar mb-3">
-                            <h2 className="text-white font-weight-normal text-center">{t('superblocks.govdetailtable.title')}</h2>
-                        </div>
-                        <div className="table-responsive">
-                            <table className="table live__table table-bordered table-dark table-striped">
-                                <thead className="table-heading">
-                                    <tr>
-                                        <th>{t('superblocks.govdetailtable.label')}</th>
-                                        <th>{t('superblocks.govdetailtable.value')}</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="table-body">
-                                    <tr>
-                                        <td>{t('superblocks.govdetailtable.current_superblock')}</td>
-                                        <td>{this.state.superBlockData.next_superblock}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>{t('superblocks.govdetailtable.superblock_budget')}</td>
-                                        <td>{this.state.superBlockData.budget}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>{t('superblocks.govdetailtable.requested_superblock_budget')}</td>
-                                        <td>{this.state.budgetSum}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>{t('superblocks.govdetailtable.superblock_date_utc')}</td>
-                                        <td>{this.state.superBlockData.superblock_date}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>{t('superblocks.govdetailtable.voting_deadline_utc')}</td>
-                                        <td>{this.state.superBlockData.voting_deadline}</td>
                                     </tr>
                                 </tbody>
                             </table>
