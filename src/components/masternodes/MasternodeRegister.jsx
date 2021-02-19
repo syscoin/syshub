e05cdeb960e931f19a2c9bbdd04d1a1531881393
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-02-18 22:51:41 -0400

    create commands masternode register.

--- src/components/masternodes/MasternodeRegister.jsx
+++ src/components/masternodes/MasternodeRegister.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import Title from '../global/Title';
 import MasternodeForm from './MasternodeForm';
 
 
 export const MasternodeRegister = () => {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
+
+  const generatePrepareCommand = (collateralHash, collateralIndex, ipAddress, ownerKeyAddr, operatorPubKey, votingKeyAddr, operatorReward, payoutAddress, feeSourceAddress) => {
+    return `protx_register_prepare ${collateralHash} ${collateralIndex} ${ipAddress}:18369 ${ownerKeyAddr} ${operatorPubKey} ${votingKeyAddr} ${votingKeyAddr} ${operatorReward} ${payoutAddress} ${feeSourceAddress || ''}`.trim();
+  }
+
+  const generateSingCommand = (collateralAddress, signMessage) => {
+    if (collateralAddress.startsWith('sys') || collateralAddress.startsWith('tsys')) {
+      return `signmessagebech32 ${collateralAddress} ${signMessage}`.trim();
+    } else {
+      return `signmessage ${collateralAddress} ${signMessage}`.trim();
+    }
+  }
+
+  const generateSubmitCommand = (tx, sig) => {
+    return `protx_register_submit ${tx} ${sig}`.trim();
+  }
 
   return (
     <>
-      <Title heading={t("check.register.title")} />
-      <MasternodeForm />
+      <Title heading={t("check.register.title")}/>
+      <MasternodeForm/>
     </>
   )
 }
