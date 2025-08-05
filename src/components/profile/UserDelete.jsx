ecd19b1b11b9e1aac41b583f3aaf819376fb520d
Ted Ian Osias <ted@syscoin.org>
2025-08-05 21:08:57 +0800

    fix(user-delete): user_id is replaced with uid SYSHUB001v1-ER18

--- src/components/profile/UserDelete.jsx
+++ src/components/profile/UserDelete.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import swal from "sweetalert2";
 
-import {useUser} from '../../context/user-context';
-import {deleteUser, get2faInfoUser} from '../../utils/request';
+import { useUser } from "../../context/user-context";
+import { deleteUser, get2faInfoUser } from "../../utils/request";
 import CustomModal from "../global/CustomModal";
 import Modal2FA from "./2FA/Modal2FA";
 
@@ -16,10 +16,13 @@ import Modal2FA from "./2FA/Modal2FA";
  * )
  */
 function UserDelete() {
-  const {user, logoutUser} = useUser();
+  const { user, logoutUser } = useUser();
   const [userSignInGAuth, setUserSignInGAuth] = useState(null);
   const [user2FA, setUser2FA] = useState(null);
   const [open2FAModal, setOpen2FAModal] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  console.log({ user });
 
   /**
    * function to confirm the deletion of the account and check the 2fa auth to open the modal
@@ -27,19 +30,20 @@ function UserDelete() {
    */
   const verificateDelete = async () => {
     const result = await swal.fire({
-      title: 'Your account will be deleted.',
+      title: "Your account will be deleted.",
       text: "This action cannot be undone.",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Yes, delete my account',
+      confirmButtonText: "Yes, delete my account",
     });
     if (result.isConfirmed) {
       try {
-        let user2fa = await get2faInfoUser(user.data.user_id);
+        setIsDeleting(true);
+        let user2fa = await get2faInfoUser(user.data.uid);
         if (user2fa.twoFa === true) {
           setUser2FA(user2fa);
           if (user2fa.gAuth === true) {
-            setUserSignInGAuth({secret: user2fa.gAuthSecret});
+            setUserSignInGAuth({ secret: user2fa.gAuthSecret });
           }
           setOpen2FAModal(true);
         } else {
@@ -47,13 +51,13 @@ function UserDelete() {
         }
       } catch (error) {
         swal.fire({
-          title: 'There was an error',
-          icon: 'error',
-          text: 'please try again'
+          title: "There was an error",
+          icon: "error",
+          text: "please try again",
         });
       }
     }
-  }
+  };
 
   /**
    * function to delete the account after the verification
@@ -61,45 +65,62 @@ function UserDelete() {
    */
   const deleteAccountAfter2FA = async () => {
     setOpen2FAModal(false);
-    await deleteUser(user.data.user_id).then(async () => {
-      await swal.fire({
-        title: 'Deleted',
-        text: "Your account has been deleted",
-        icon: 'success',
-        showConfirmButton: false,
-        timer: 2000
-      }).then(async () => {
-        await logoutUser();
+    await deleteUser(user.data.uid)
+      .then(async () => {
+        await swal
+          .fire({
+            title: "Deleted",
+            text: "Your account has been deleted",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 2000,
+          })
+          .then(async () => {
+            await logoutUser();
+          });
       })
-    }).catch(error => {
-      swal.fire({
-        title: 'There was an error',
-        icon: 'error',
-        text: error.message,
+      .catch((error) => {
+        swal.fire({
+          title: "There was an error",
+          icon: "error",
+          text: error.message,
+        });
+      })
+      .finally(() => {
+        setIsDeleting(false);
       });
-    });
-  }
+  };
 
   return (
     <div className="input-form">
       <div className="form-group spacer line"></div>
       <div className="form-group">
         <label className="big">Delete Account</label>
-        <br/>
-        <div className="indicator red">Your account will be deleted. This action cannot be undone.</div>
-        <div className="description">We'll never share your email with anyone else.</div>
-        <button className="btn btn--blue" onClick={verificateDelete}>Delete account</button>
+        <br />
+        <div className="indicator red">
+          Your account will be deleted. This action cannot be undone.
+        </div>
+        <div className="description">
+          We'll never share your email with anyone else.
+        </div>
+        <button
+          className="btn btn--blue"
+          onClick={verificateDelete}
+          disabled={isDeleting}
+        >
+          {isDeleting ? "Deleting..." : "Delete account"}
+        </button>
       </div>
 
-      <CustomModal
-        open={open2FAModal}
-        onClose={() => setOpen2FAModal(false)}>
-        {user2FA && <Modal2FA
-          user2fa={user2FA}
-          userSignInGAuth={userSignInGAuth}
-          onGAuth={deleteAccountAfter2FA}
-          onPhoneSMS={deleteAccountAfter2FA}
-        />}
+      <CustomModal open={open2FAModal} onClose={() => setOpen2FAModal(false)}>
+        {user2FA && (
+          <Modal2FA
+            user2fa={user2FA}
+            userSignInGAuth={userSignInGAuth}
+            onGAuth={deleteAccountAfter2FA}
+            onPhoneSMS={deleteAccountAfter2FA}
+          />
+        )}
       </CustomModal>
     </div>
   );
